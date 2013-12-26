36e680c22ed00f5a40a715ceb3046b2c3e1a9fe7
Christopher Métrailler <christopher.metrailler@master.hes-so.ch>
2013-12-26 23:11:42 +0100

    Bezier curve algorithm added. The number of points of the curve is generic.

--- lib/ihmcurve/ihmcurve.cpp
+++ lib/ihmcurve/ihmcurve.cpp
@@ -1,28 +1,60 @@
-/**
- * @brief Constructor/destructor functions definition
- * @file ihmcurve.cpp
- * @author ...
- * @date Dec 2013
- */
-
-
 #include "ihmcurve.h"
-
 
 /**
  * Constructor
  */
 CIHMCurve::CIHMCurve(void)
 {
-
+	// Not used...
 }
 
+
+/**
+ * Generates a number of points that correspond to a Bezier curve.
+ *
+ * @param fixedPoints IN list of fixed points of the curve
+ * @param curvePoints OUT Bezier curve points
+ * @param nbrBezierPoints IN Number of Bezier points to generate
+ */
+void CIHMCurve::curveGenerator(vector<Point> *fixedPoints, Point *curvePoints, int nbrBezierPoints) {
+	float step = 0.0;
+
+	// Do it for all Bezier points (typically 1000 points)
+	for(int idx = 0; idx < nbrBezierPoints; idx ++) {
+		curvePoints[idx] = pointGenerator(fixedPoints, step);
+		step = float(step + 1.0f / float(nbrBezierPoints));
+	}
+}
+
+/**
+ * Bezier Curve algorithm.
+ *
+ * @param bezierPoints IN list of fixed points
+ * @param step IN step of Bezier algorithm
+ */
+Point CIHMCurve::pointGenerator(vector<Point> *bezierPoints, float step) {
+	if (bezierPoints->size() == 1) {
+		return bezierPoints->at(0);	// End
+	} else {
+		vector<Point> temp;
+		unsigned int j = 0;
+		while (j < (bezierPoints->size() - 1)) {
+			// Calculate point coordinates
+			int ptX = (int) ((1.0 - step) * bezierPoints->at(j).x + step * bezierPoints->at(j + 1).x);
+			int ptY = (int) ((1.0 - step) * bezierPoints->at(j).y + step * bezierPoints->at(j + 1).y);
+			temp.push_back(Point(ptX, ptY));
+			j++;
+		}
+		// Recursive algorithm
+		return pointGenerator(&temp, step);
+	}
+}
 
 /**
  * Destructor
  */
 CIHMCurve::~CIHMCurve(void)
 {
-
+	// Not used...
 }
 
--- lib/ihmcurve/ihmcurve.h
+++ lib/ihmcurve/ihmcurve.h
@@ -1,28 +1,35 @@
 /**
- * @brief Class header file
- * @file ihmcurve.h
- * @author ...
- * @date Dec 2013
+ * @brief	CIHMCurve class header file
+ * 			Bezier curve algorithm. The number of points for the Bezier curve
+ * 			can be set. Algorithm info here: http://en.wikipedia.org/wiki/B%C3%A9zier_curve
+ * @file	ihmcurve.h
+ *
+ * @version	1.0
+ * @date	26 December 2013
+ * @author	Rodolfo Horta (rodolfo.horta@master.hes-so.ch)
+ * @author	Christopher Métrailler (christopher.metrailler@master.hes-so.ch)
  */
 #ifndef _IHM_CURVE_H_
 #define _IHM_CURVE_H_
 
 #include <iostream>
-#include <sstream>
+#include <ostream>
 
+// OpenCV
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
-
-
-#define kCIHMCurve_VERSION_MAJOR_NUMBER	(0)
-#define kCIHMCurve_VERSION_MINOR_NUMBER	(1)
-#define kCIHMCurve_VERSION_PATCH_NUMBER	(1)
-
-
 
 using namespace std;
 using namespace cv;
 
+// Version 1.0.0, December 2013
+
+/** Major number */
+#define kCIHMCurve_VERSION_MAJOR_NUMBER	(1)
+/** Minor number */
+#define kCIHMCurve_VERSION_MINOR_NUMBER	(0)
+/** Patch number */
+#define kCIHMCurve_VERSION_PATCH_NUMBER	(0)
 
 /**
  * CIHMCurve Class 
@@ -30,12 +37,13 @@ using namespace cv;
 class CIHMCurve
 {
 public:
-	/*
-	 * PUBLIC Functions
-	 */
 	// Constructor/destructor
 	CIHMCurve(void);
 	~CIHMCurve(void);
+
+
+	// List of Bezier Points
+	void curveGenerator(vector<Point> *fixedPoints, Point *curvePoints, int nbrBezierPoints);
 
 	// Get/set
 	string version_get(void) const;
@@ -44,10 +52,9 @@ public:
 	friend ostream& operator<<(ostream& out, const CIHMCurve& curve);
 
 private:
-	/*
-	 * PRIVATE Objects
-	 */
 
+	// Bezier algorithme for 1 Point generation
+	Point pointGenerator(vector<Point> *bezierPoints, float step);
 
 }; // CIHMCurve
 
